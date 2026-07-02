4498e4af240970802f99bc0ee2bcedf6accc1dfa
Hamida Badamdi <hamida.badamdi143121@marwadiuniversity.ac.in>
2026-07-02 14:56:03 +0530

    feat: add train-test split using scikit-learn
    ### Train-Test Split
    
    This practical demonstrates splitting the Wine dataset into training and testing sets using `train_test_split()`.

---
+++ 14-train-test-split.py
@@ -0,0 +1,38 @@
+"""
+14- Train-Test Split
+
+Description:
+This program demonstrates how to split a dataset into training and testing
+sets using scikit-learn. The Wine dataset is loaded, features and target
+labels are separated, and the data is divided for model training and evaluation.
+
+Key Concepts:
+- Dataset Splitting
+- Training Set
+- Testing Set
+- train_test_split()
+
+"""
+
+#Spliting dataset in to training and testing datasets
+import numpy as np
+import pandas as pd
+#from sklearn.cross_validation import train_test_split is deprecated
+from sklearn.model_selection import train_test_split
+
+df_wine=pd.read_csv('D:/Winter 2023/BSc DS 4/Programs/wine.csv')
+print(df_wine)
+
+print('Cultivars', np.unique(df_wine['Cultivars']))
+
+print(df_wine.head())
+
+x=df_wine.iloc[:,1:].values
+print(x)
+
+y=df_wine.iloc[:,0].values
+print(y)
+
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.3,random_state=0)
+
+
